c10d10f1649491a67720e9c1d0802c183d758a85
Lê Cường Quốc <157682354+cuongquoc123@users.noreply.github.com>
2026-08-24 08:59:40 +0700

    chore: initial project

---
+++ bookbe/src/main/java/com/example/bookbe/BookbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookbeApplication.class, args);
+	}
+
+}
---
+++ bookbe/src/test/java/com/example/bookbe/BookbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
